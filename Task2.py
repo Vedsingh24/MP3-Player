c7eb5380a89796f5dd952866e0db55b8a6af14db
Vedsingh24 <vedstreng@gmail.com>
2026-06-29 12:09:40 +0530

    Started work on the backend function (demuxing, extracting audio streams and converting things into arrays)

--- Task2.py
+++ Task2.py
@@ -1,13 +1,14 @@
 import mutagen as mt
 from pathlib import Path
+import av
 
 class Metadataextractor:
     def __init__(self):
-        self.path = None
+        self.path = r"C:\Users\Ved Singh\Downloads\Alone_-_Color_Out.mp3"
         self.metadata = None
-        self.path_config()
-        self.print_path()
-        self.metadata_extractor()
+        # self.path_config()
+        # self.metadata_extractor()
+
 
     def path_config(self):
         path_input = input("Paste in your mp3 file path here:(absolute)")
@@ -24,8 +25,41 @@ class Metadataextractor:
                          "Website":audio['TPUB'].text[0]}
         print(self.metadata)
 
+metadata = Metadataextractor()
+
+class Mp3backend:
+    def __init__(self):
+        self.path= metadata.path
+        self.metadeta = metadata.metadata
+        self.decoded_sample = None
+        self.audio_stream = None
+
+        self.audio_stream_data = None
+        self.audio_stream_extraction()
+        self.packet_extraction()
+
+
+    def audio_stream_extraction(self):
+        self.decoded_sample=av.open(self.path)
+        for streams in self.decoded_sample.streams:
+            print(streams)
+            self.audio_stream= self.decoded_sample.streams.audio[0]
+            self.audio_stream_data = {"codec context":self.audio_stream.codec_context,"sample rate":self.audio_stream.sample_rate,"channels":self.audio_stream.channels}
+            print(self.audio_stream_data)
+
+    def packet_extraction(self):
+        for packets in self.decoded_sample.container(self.audio_stream):
+            print(packets)
 
 
 
 
-Metadataextractor()
+
+array_extract = Mp3backend()
+
+
+
+
+
+
+
