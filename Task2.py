f394775ce1ebe93b31bea55e9555a103244afbbf
Vedsingh24 <vedstreng@gmail.com>
2026-07-09 19:56:18 +0530

    Added frontend skeleton and finished the equalization feature on the backend side

--- Task2.py
+++ Task2.py
@@ -5,15 +5,18 @@ import numpy as np
 import sounddevice as sd
 import scipy as sc
 from scipy import signal
+from tkinter import *
+from tkinter import ttk
+import tkinter as Tk
 
 
 
 class Metadataextractor:
     def __init__(self):
-        self.path = r"C:\Users\Ved Singh\Downloads\Alone_-_Color_Out.mp3"
+        self.path = None
         self.metadata = None
-        # self.path_config()
-        # self.metadata_extractor()
+        self.path_config()
+        self.metadata_extractor()
 
 
     def path_config(self):
@@ -42,21 +45,22 @@ class Mp3backend:
         self.pcm_chunks = []
         self.pcm = None
         self.full_audio = None
-        self.volume = 1.0
-        self.speed = 1.0
+        self.volume = None
+        self.speed = None
         self.sample_rate = None
         self.bass_audio = None
         self.low_mid_audio = None
         self.high_mid_audio = None
         self.treble_audio = None
-        self.bass_vol = 1.2
-        self.lowmid_vol = 1.1
-        self.highmid_vol = 0.9
-        self.treble_vol = 0.2
+        self.bass_vol = None
+        self.lowmid_vol = None
+        self.highmid_vol = None
+        self.treble_vol = None
 
 
 
         self.audio_stream_data = None
+        self.frontend_value_coll()
         self.audio_stream_extraction()
         self.array_extraction()
 
@@ -93,13 +97,13 @@ class Mp3backend:
         self.bass_filter = signal.butter(4,250,'low',output='sos',fs=self.sample_rate)
         self.low_mid_filter = signal.butter(N=4,Wn=[250,1000],btype="bandpass",output='sos',fs=self.sample_rate)
         self.high_mid_filter = signal.butter(N=4, Wn=[1000, 4000], btype="bandpass", output='sos', fs=self.sample_rate)
-        self.treble_filter = signal.butter(N=4, Wn=[4000, 20000], btype="bandpass", output='sos', fs=self.sample_rate)
+        self.treble_filter = signal.butter(N=4, Wn=4000, btype="high", output='sos', fs=self.sample_rate)
     def equalization(self):
         self.filterdesign()
-        self.bass_audio = signal.sosfiltfilt(self.bass_filter,self.full_audio)*self.bass_vol
-        self.low_mid_audio = signal.sosfiltfilt(self.low_mid_filter,self.full_audio)*self.lowmid_vol
-        self.high_mid_audio = signal.sosfiltfilt(self.high_mid_filter,self.full_audio)*self.highmid_vol
-        self.treble_audio = signal.sosfiltfilt(self.treble_filter,self.full_audio)*self.treble_vol
+        self.bass_audio = signal.sosfiltfilt(self.bass_filter,self.full_audio,axis=0)*self.bass_vol
+        self.low_mid_audio = signal.sosfiltfilt(self.low_mid_filter,self.full_audio,axis=0)*self.lowmid_vol
+        self.high_mid_audio = signal.sosfiltfilt(self.high_mid_filter,self.full_audio,axis=0)*self.highmid_vol
+        self.treble_audio = signal.sosfiltfilt(self.treble_filter,self.full_audio,axis=0)*self.treble_vol
         self.full_audio = self.bass_audio + self.low_mid_audio + self.high_mid_audio + self.treble_audio
 
 
@@ -109,9 +113,12 @@ class Mp3backend:
 
     def audio_streaming(self):
         self.full_audio = np.concatenate(self.pcm_chunks,axis=0)
+        self.speed_set()
+        self.equalization()
+
 
         self.volume_set()
-        self.speed_set()
+
         play = sd.play(self.full_audio,samplerate=self.sample_rate)
         sd.wait()
 
@@ -133,6 +140,37 @@ class Mp3backend:
 
 
 array_extract = Mp3backend()
+
+class Mp3Frontend:
+    def __init__(self):
+        self.root = Tk()
+        self.root.geometry("500x500")
+        self.frame = ttk.Frame(self.root)
+        self.gui_creation()
+        self.gui_placement()
+        self.root.mainloop()
+
+
+    def gui_creation(self):
+        self.play_button = ttk.Button(self.root,image="play_button.png",command=None)
+        self.volume_scale = Tk.Scale(self.root,from_=0,to=200,resolution=1,orient="horizontal")
+        self.bass_scale = Tk.Scale(self.root,from_=0,to=200,resolution=1,orient="horizontal")
+        self.low_mid_scale = Tk.Scale(self.root,from_=0,to=200,resolution=1,orient="horizontal")
+        self.high_mid_scale = Tk.Scale(self.root,from_=0,to=200,resolution=1,orient="horizontal")
+        self.treble_scale = Tk.Scale(self.root,from_=0,to=200,resolution=1,orient="horizontal")
+        self.speed_scale = Tk.Scale(self.root,from_=0,to=3,resolution=0.25,orient="horizontal")
+        self.browse_mp3 = ttk.Button(self.root, image="browse_button.png",command = None)
+
+
+    def gui_placement(self):
+        self.play_button.place(150,200)
+
+
+
+
+
+
+
 
 
 
